409b3f9ad85a53a72a7945a8a4b7329174f01630
Wiktormisch <wiktor.misch@gmail.com>
2026-04-13 12:32:35 +0200

    feat: Movie model

---
+++ Movies/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Movie (models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    descritpion = models.TextField(blank=True, null=True)
+    director = models.TextField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
