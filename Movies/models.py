a36c10f46be6dfb8b38d0de196f6deb7e27b6016
Wiktormisch <wiktor.misch@gmail.com>
2026-04-14 11:00:30 +0200

    feat: update parameters in Movie model

--- Movies/models.py
+++ Movies/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 
 class Movie (models.Model):
+
+    STATUS_CHOICES = [("watched", "Watched"),
+                      ("to_watch", "To Watch")]
+
     title = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
-    director = models.TextField(blank=True, null=True)
+    director = models.CharField(max_length=255, blank=True, null=True)
     year = models.IntegerField(blank=True, null=True)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="to_watch")
+    created_at = models.DateField(auto_now_add=True, blank=True)
+    updated_at = models.DateField(auto_now=True, blank=True)
 
     def __str__(self):
-        return self.title
+
+        return f"{self.title} - {self.year}"
