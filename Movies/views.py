070df40e64e71f433d9043525f825fe19e9c9318
Wiktormisch <wiktor.misch@gmail.com>
2026-04-14 09:49:56 +0200

    feat: add form and view for new movie

--- Movies/views.py
+++ Movies/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from .models import Movie
+from .forms import MovieForm
 
 
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, "movies/movie_list.html", {"movies": movies})
+
+
+def add_movie(request):
+    if request.method == "POST":
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = MovieForm()
+
+    return render(request, "movies/add_movie.html", {"form": form})
