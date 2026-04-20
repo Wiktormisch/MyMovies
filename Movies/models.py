b0edb2ae21e1c954fc1ca8744ea748f7d72cc4fa
Wiktormisch <wiktor.misch@gmail.com>
2026-04-20 14:10:21 +0200

    feat: add tag and rating to model

--- Movies/models.py
+++ Movies/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Tag (models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+
+        return self.name
+
+
 class Movie (models.Model):
 
     STATUS_CHOICES = [("watched", "Watched"),
                       ("to_watch", "To Watch")]
+    RAITING_CHOICES = [(i, str(i)) for i in range(1, 11)]
 
     title = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
@@ -13,6 +22,9 @@ class Movie (models.Model):
     year = models.IntegerField(blank=True, null=True)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default="to_watch")
+    rating = models.IntegerField(
+        max_length=2, choices=RAITING_CHOICES, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     created_at = models.DateField(auto_now_add=True, blank=True)
     updated_at = models.DateField(auto_now=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
